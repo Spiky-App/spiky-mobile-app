fd85c689ebfb9e0cc9f84594df8189c308bda614
Raul Llamas <rauldllamas@gmail.com>
2022-07-24 15:20:27 -0500

    Avance Loading animacion

--- src/screens/CommunityScreen.tsx
+++ src/screens/CommunityScreen.tsx
@@ -8,28 +8,38 @@ import { EmptyState } from '../components/EmptyState';
 import { ButtonMoreIdeas } from '../components/ButtonMoreIdeas';
 import { useSelector } from 'react-redux';
 import { State } from '../store/reducers';
+import { Text } from 'react-native-svg';
+import { LoadingSvg } from '../components/LoadingSvg';
+
 
 export const CommunityScreen = () => {
   const { mensajes } = useSelector((state: State) => state.message);
-  const loading = false;
+  const loading = true;
   const moreMsg = true;
 
   return (
     <BackgroundPaper style={{ justifyContent: 'flex-start' }}>
       <IdeasHeader title="Comunidad" />
 
-      {mensajes && !loading ? (
-        <FlatList
-          style={{ width: '90%' }}
-          data={mensajes}
-          renderItem={({ item }) => <Idea idea={item} />}
-          keyExtractor={item => item.id_mensaje + ''}
-          showsVerticalScrollIndicator={false}
-          ListFooterComponent={moreMsg ? ButtonMoreIdeas : <></>}
-        />
-      ) : (
-        <EmptyState message="Cuestión de tiempo de que alguien hable." />
-      )}
+      { !loading ? 
+          (
+            mensajes ? (
+              <FlatList
+                style={{ width: '90%' }}
+                data={mensajes}
+                renderItem={({ item }) => <Idea idea={item} />}
+                keyExtractor={item => item.id_mensaje + ''}
+                showsVerticalScrollIndicator={false}
+                ListFooterComponent={moreMsg ? ButtonMoreIdeas : <></>}
+              />
+            ) : (
+              <EmptyState message="Cuestión de tiempo de que alguien hable." />
+            )
+          )
+        :
+          <LoadingSvg />
+      }
+
 
       <FloatButton />
     </BackgroundPaper>
