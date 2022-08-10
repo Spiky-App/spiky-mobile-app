a6e0d8389512c8c2ce05b47048c65c07ec6f92a5
Fer Gallo <mafergallocruz@gmail.com>
2022-08-10 18:23:28 -0500

    use new flatlist component

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,17 +1,13 @@
 import React, { useEffect } from 'react';
-import { FlatList } from 'react-native';
 import { BackgroundPaper } from '../components/BackgroundPaper';
 import { IdeasHeader } from '../components/IdeasHeader';
-import { Idea } from '../components/Idea';
 import { FloatButton } from '../components/FloatButton';
-import { EmptyState } from '../components/EmptyState';
-import { ButtonMoreIdeas } from '../components/ButtonMoreIdeas';
-import { LoadingAnimated } from '../components/svg/LoadingAnimated';
 import { useAppDispatch } from '../store/hooks';
 import { setFilter } from '../store/feature/messages/messagesSlice';
 import { useMensajes } from '../hooks/useMensajes';
 import { DrawerParamList } from '../navigator/MenuMain';
 import { DrawerScreenProps } from '@react-navigation/drawer';
+import MessagesFeed from '../components/MessagesFeed';
 
 type Props = DrawerScreenProps<DrawerParamList, 'ProfileScreen'>;
 
@@ -24,29 +20,12 @@ export const ProfileScreen = ({ route }: Props) => {
     useEffect(function () {
         dispatch(setFilter('/perfil'));
     }, []);
-    const { messages, loading, moreMsg } = useMensajes({ alias });
+    const { messages } = useMensajes({ alias });
 
     return (
         <BackgroundPaper style={{ justifyContent: 'flex-start' }}>
             <IdeasHeader title={'@' + alias} />
-
-            {messages?.length !== 0 ? (
-                <FlatList
-                    style={{ width: '90%' }}
-                    data={messages}
-                    renderItem={({ item }) => <Idea idea={item} />}
-                    keyExtractor={item => item.id + ''}
-                    showsVerticalScrollIndicator={false}
-                    ListFooterComponent={
-                        loading ? LoadingAnimated : moreMsg ? ButtonMoreIdeas : <></>
-                    }
-                    ListFooterComponentStyle={{ marginVertical: 12 }}
-                />
-            ) : loading ? (
-                <LoadingAnimated />
-            ) : (
-                <EmptyState message="" />
-            )}
+            <MessagesFeed messages={messages} />
 
             <FloatButton />
         </BackgroundPaper>
