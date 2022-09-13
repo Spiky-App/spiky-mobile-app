3869072440408d75cee830ca33bcc96f7c316363
Raul Llamas <rauldllamas@gmail.com>
2022-09-12 22:07:12 -0500

    commit

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,14 +1,14 @@
 import { AxiosRequestConfig } from 'axios';
-// import { BASE_URL, PORT } from 'react-native-dotenv';
+import { BASE_URL, PORT } from 'react-native-dotenv';
 
-// export const socketBaseUrl: string = `${BASE_URL}:${PORT}`;
-export const socketBaseUrl: string = `https://spiky-services.herokuapp.com`;
+export const socketBaseUrl: string = `${BASE_URL}:${PORT}`;
+// export const socketBaseUrl: string = `https://spiky-services.herokuapp.com`;
 // this export is required to work with android emulator
 // i'm searching for alternatives but for now it hasn't
 // hurt anyone
 export const config: AxiosRequestConfig = {
-    // baseURL: `${BASE_URL}:${PORT}/api/`,
-    baseURL: `https://spiky-services.herokuapp.com/api/`,
+    baseURL: `${BASE_URL}:${PORT}/api/`,
+    // baseURL: `https://spiky-services.herokuapp.com/api/`,
     timeout: 10000,
     headers: {},
 };
