55bc71f4c7649bcec971908ac703c018bf89a902
Fer Gallo <mafergallocruz@gmail.com>
2022-10-15 19:47:51 -0500

    renamed app

--- ios/SpikyAppTests/SpikyAppTests.m
+++ ios/SpikyAppTests/SpikyAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeCliAppTests : XCTestCase
+@interface SpikyAppTests : XCTestCase
 
 @end
 
-@implementation reactNativeCliAppTests
+@implementation SpikyAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
