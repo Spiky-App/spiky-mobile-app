1dff1acc960ad72ceb8d78162f021680c5e238ee
Fer Gallo <mafergallocruz@gmail.com>
2022-11-05 17:52:32 -0600

    fix: console logs removal

--- src/services/NotificationService.ts
+++ src/services/NotificationService.ts
@@ -53,7 +53,6 @@ class NotificationService {
 
             // (required) Called when a remote is received or opened, or local notification is opened
             onNotification: function (notification) {
-
                 // process the notification
                 if (notification.userInteraction) {
                     const { data } = notification;
