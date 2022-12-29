34a4816ff75171351d76fefe1824fcd4025d7cd5
Raul Llamas <rauldllamas@gmail.com>
2022-12-28 18:18:07 -0600

    fix: remove api from prod

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 import { AxiosRequestConfig } from 'axios';
 
-export const socketBaseUrl: string = `https://spiky-services-prod.herokuapp.com`;
+export const socketBaseUrl: string = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`;
 // this export is required to work with android emulator
 // i'm searching for alternatives but for now it hasn't
 // hurt anyone
 export const config: AxiosRequestConfig = {
-    baseURL: `https://spiky-services-prod.herokuapp.com/api`,
+    baseURL: `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/api`,
     timeout: 10000,
     headers: {},
 };
