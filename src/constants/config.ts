52234964a3a3c3523f7902b67ee69a8314d76aa5
Raul Llamas <rauldllamas@gmail.com>
2023-01-16 16:59:31 -0600

    fix: catch error in messaging getToken function

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 import { AxiosRequestConfig } from 'axios';
 
-export const socketBaseUrl: string = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`;
+export const socketBaseUrl: string = `https://spiky-services-prod.herokuapp.com`;
 // this export is required to work with android emulator
 // i'm searching for alternatives but for now it hasn't
 // hurt anyone
 export const config: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/api`,
-    timeout: 10000,
+    baseURL: `https://spiky-services-prod.herokuapp.com/api`,
+    timeout: 7000,
     headers: {},
 };
