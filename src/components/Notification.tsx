927670de8823b633b3ce05bea31cb150c955089a
Fer Gallo <mafergallocruz@gmail.com>
2023-01-19 23:34:14 -0600

    fix: misspelled words fix

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -22,9 +22,9 @@ const msg_notif = [
     'respondió en tu tracking.',
     'te mencionó.',
     'reaccionó a tu comentario.',
-    'reacciono en tu tracking.',
-    'contesto tu encuesta.',
-    'conntesto en tu tracking.',
+    'reaccionó en tu tracking.',
+    'contestó tu encuesta.',
+    'contestó en tu tracking.',
 ];
 
 export const Notification = ({ notification, setModalNotif }: PropsNotification) => {
