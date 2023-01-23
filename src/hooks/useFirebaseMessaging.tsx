b5bd0970d6ad9999bca8f04348cd1aa6e48ac973
Fer Gallo <mafergallocruz@gmail.com>
2023-01-22 21:33:46 -0600

    update: refactored firebase messaging logic

--- src/hooks/useFirebaseMessaging.tsx
+++ src/hooks/useFirebaseMessaging.tsx
@@ -1,51 +1,52 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { firebase } from '@react-native-firebase/messaging';
+//import { firebase } from '@react-native-firebase/messaging';
 import { useCallback } from 'react';
 import { Alert } from 'react-native';
 import { StorageKeys } from '../types/storage';
 import useSpikyService from './useSpikyService';
-
+import { Platform } from 'react-native';
+import Messaging from '@react-native-firebase/messaging';
 export const useFirebaseMessaging = () => {
     const { getNetworkConnectionStatus } = useSpikyService();
 
+    const getToken = () => {
+        let token;
+        Messaging()
+            .getToken()
+            .then(async tokenDevice => {
+                token = tokenDevice;
+                console.log(token);
+            })
+            .catch(error => {
+                let err = `FCm token get error${error}`;
+                Alert.alert(err);
+                console.log(err);
+            });
+        return token;
+    };
     const getTokenDevice = useCallback(async () => {
         const networkConnectionStatus = await getNetworkConnectionStatus();
         if (networkConnectionStatus) {
             let token;
-            try {
-                token = await firebase
-                    .messaging()
-                    .getToken()
-                    .catch(error => {
-                        let err = `FCm token get error${error}`;
-                        Alert.alert(err);
+            if (Platform.OS === 'ios') {
+                Messaging()
+                    .requestPermission()
+                    .then(status => {
+                        const enabled =
+                            status === Messaging.AuthorizationStatus.AUTHORIZED ||
+                            status === Messaging.AuthorizationStatus.PROVISIONAL;
+                        if (enabled) {
+                            token = getToken();
+                        }
                     });
-                if (token) await AsyncStorage.setItem(StorageKeys.DEVICE_TOKEN, token);
-
-                // Listen to whether the token changes
-                let tokenRefreshListenerUnsubscriber = firebase
-                    .messaging()
-                    .onTokenRefresh(async fcmToken => {
-                        console.log(tokenRefreshListenerUnsubscriber);
-                        await AsyncStorage.setItem(StorageKeys.DEVICE_TOKEN, fcmToken);
-                    });
-            } catch (e) {
-                console.log(e);
-                try {
-                    await firebase.messaging().requestPermission();
-                    token = await firebase
-                        .messaging()
-                        .getToken()
-                        .catch(error => {
-                            let err = `FCm token get error${error}`;
-                            Alert.alert(err);
-                        });
-                    if (token) await AsyncStorage.setItem(StorageKeys.DEVICE_TOKEN, token);
-                } catch (error) {
-                    console.log(error);
-                    throw error;
-                }
+            } else if (Platform.OS === 'android') {
+                token = getToken();
             }
+            if (token) await AsyncStorage.setItem(StorageKeys.DEVICE_TOKEN, token);
+            // Listen to whether the token changes
+            Messaging().onTokenRefresh(async fcmToken => {
+                await AsyncStorage.setItem(StorageKeys.DEVICE_TOKEN, fcmToken);
+            });
         }
     }, []);
 
