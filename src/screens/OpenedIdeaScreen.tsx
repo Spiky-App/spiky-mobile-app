dfa8802cc94f79bbca01eac55767d75af81295e8
Raul Llamas <rauldllamas@gmail.com>
2023-01-22 18:47:40 -0600

    fix: go back on error loading idea with comments

--- src/screens/OpenedIdeaScreen.tsx
+++ src/screens/OpenedIdeaScreen.tsx
@@ -84,6 +84,8 @@ export const OpenedIdeaScreen = ({ route: routeSC }: Props) => {
             setComments(messageRetrived.comments ?? []);
             setMessageTrackingId(messageRetrived.messageTrackingId);
             setAnswersNumber(messageRetrived.comments?.length || 0);
+        } else {
+            navigation.navigation.goBack();
         }
         setLoading(false);
     };
