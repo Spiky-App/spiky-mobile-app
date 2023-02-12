9248cb1e0229bcb33cd734e2966c1b149fa44b6f
Felipe López Valdez <0204021@up.edu.mx>
2023-02-11 21:49:08 -0700

    Add Spiky notification icon

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 import { AxiosRequestConfig } from 'axios';
 
-export const socketBaseUrl: string = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}`;
+export const socketBaseUrl: string = `${process.env.REACT_APP_BASE_URL}`;
 // this export is required to work with android emulator
 // i'm searching for alternatives but for now it hasn't
 // hurt anyone
 export const config: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/api`,
+    baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
     timeout: 8000,
     headers: { 'Content-Type': 'application/json' },
 };
